51129f7b76507e2fef726117e60637d894a1e3fb
Herbert Cuyugan <herbert.cuyugan@pentanasolutions.com>
2023-05-01 00:38:38 +1000

    snake game movements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,19 +9,29 @@ screen.title("My Snake Game")
 
 # for the snake to follow each one without delay
 screen.tracer(0)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
 
 # better ways of doing the below positioning using tuple
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+# starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 # snake to move
-snakes = []
+segments = []
 
-for position in starting_positions:
-    new_snake = Turtle("square")
-    new_snake.color("white")
-    new_snake.penup()
-    new_snake.goto(position)
-    snakes.append(new_snake)
+# for position in starting_positions:
+#     new_segment = Turtle("square")
+#     new_segment.color("white")
+#     new_segment.penup()
+#     new_segment.goto(position)
+#     segments.append(new_segment)
 
 # snake_1 = Turtle("square")
 # snake_1.color("white")
@@ -38,10 +49,14 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for snake_num in range(len(snakes) - 1, 0, -1):
-        new_x = snakes[snake_num - 1].xcor()
-        new_y = snakes[snake_num - 1].ycor()
-        snakes[snake_num].goto(new_x, new_y)
-    snakes[0].forward(20)
+
+    snake.move()
+
+
+    # for seg_num in range(len(segments) - 1, 0, -1):
+    #     new_x = segments[seg_num - 1].xcor()
+    #     new_y = segments[seg_num - 1].ycor()
+    #     segments[seg_num].goto(new_x, new_y)
+    # segments[0].forward(20)
 
 screen.exitonclick()
